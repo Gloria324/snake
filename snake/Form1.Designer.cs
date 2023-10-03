ed789e1fcba30319a49b351cb190b3358f7a512a
glori <glori@GLORIA>
2023-10-03 11:48:24 -0300

    Agregar archivos de proyecto.

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(33, 36);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(124, 25);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "score: 0\r\n";
+            // 
+            // lblFood
+            // 
+            this.lblFood.BackColor = System.Drawing.Color.Red;
+            this.lblFood.Location = new System.Drawing.Point(396, 152);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(21, 21);
+            this.lblFood.TabIndex = 1;
+            this.lblFood.Text = "\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.lblFood);
+            this.Controls.Add(this.lblScore);
+            this.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblFood;
+    }
+}
+
